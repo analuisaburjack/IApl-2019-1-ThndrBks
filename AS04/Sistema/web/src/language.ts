dcf076d9ea1d1f7fff93bf74bf0cc4097583bd4e
Victor Stillo <victor.stillo@gmail.com>
2019-06-17 19:43:55 -0300

    language file

---
+++ AS04/Sistema/web/src/language.ts
@@ -0,0 +1,29 @@
+export class Language {
+    pt: {
+        newBookButton: "Novo Livro",
+        newBookTitle: "Titulo",
+        title: "Titulo",
+        isbn: "isbn",
+        author: "Autor",
+        release: "Lançamento",
+        genre: "Gênero",
+        cover: "Capa",
+        create: "Cadastrar",
+        editBook: "Editar Livro",
+        saveChanges: "Atualizar"
+    }
+
+    en: {
+        newBookButton: "New Book",
+        newBookTitle: "Create Book",
+        title: "Title",
+        isbn: "isbn",
+        author: "Author",
+        release: "Release",
+        genre: "Genre",
+        cover: "Cover",
+        create: "Create",
+        editBook: "Edit book",
+        saveChanges: "Update"
+    }
+}
